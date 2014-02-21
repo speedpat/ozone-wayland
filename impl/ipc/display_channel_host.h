f960b426e14feee20ced2937e86d8d02277dc4f2
Patrick Heeb <patrick.heeb@noser.com>
2014-02-21 15:40:40 +0100

    fix channel host

--- impl/ipc/display_channel_host.h
+++ impl/ipc/display_channel_host.h
@@ -31,6 +31,11 @@ class OzoneDisplayChannelHost : public content::BrowserChildProcessObserver {
                       ui::EventFlags flags,
                       float x,
                       float y);
+  void OnTouchNotify(ui::EventType type,
+                     float x,
+                     float y,
+                     int32_t touch_id,
+                     uint32_t time_stamp);
   void OnAxisNotify(float x, float y, int xoffset, int yoffset);
   void OnPointerEnter(unsigned handle, float x, float y);
   void OnPointerLeave(unsigned handle, float x, float y);
